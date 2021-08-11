2906b9ffb3a7d4e3915440ba142657eec895fcab
bathmonster <leslie.meade@gmail.com>
2021-08-11 20:16:36 +1000

    Update account_info.py

--- account_info.py
+++ account_info.py
@@ -3,8 +3,8 @@
 twilio_account_sid = 'what you do not have one'
 twilio_auth_token = 'what you do not have one'
 
-twilio_number = '+14157232947'
-target_number = '+61478765114'
+twilio_number = 'what you do not have one'
+target_number = 'what you do not have one'
 
-target_number_leslie = '+61478765114'
-target_number_trisha = '+61423211780'
+target_number_leslie = 'what you do not have one'
+target_number_trisha = 'what you do not have one'
